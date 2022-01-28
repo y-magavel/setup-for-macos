3b8599e9fe39cf71f5eb0debbde796bc5899b0b0
y-magavel <magavel.dev@gmail.com>
2022-01-29 01:36:15 +0900

    First commit

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Homebrewのインストール
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+#Google Chromeのインストール
+brew install --cask google-chrome
+
+#Visual Studio Codeのインストール
+brew install --cask visual-studio-code
+
+#Todoistのインストール
+brew install --cask todoist
+
+#Clipyのインストール
+brew install --cask clipy
+
+#iTerm2のインストール
+brew install --cask iterm2
