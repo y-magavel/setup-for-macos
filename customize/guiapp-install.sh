39d36eb0338616489eaba29a1db61f9e3d45939d
y-magavel <magavel.dev@gmail.com>
2022-01-29 02:15:11 +0900

    デスクトップアプリのインストール処理をcustomizeディレクトリ配下にファイル分割

---
+++ customize/guiapp-install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#--------------------------------------------------
+# デスクトップアプリケーションのインストール
+#
+# インストールするアプリを変更したい場合は、
+# 下記記載の公式サイトで対応アプリを検索してください。
+#
+# 検索ページ：https://formulae.brew.sh/cask/
+#--------------------------------------------------
+
+#Google Chromeのインストール
+brew install --cask google-chrome
+
+#Visual Studio Codeのインストール
+brew install --cask visual-studio-code
+
+#Todoistのインストール
+brew install --cask todoist
+
+#Clipyのインストール
+brew install --cask clipy
+
+#iTerm2のインストール
+brew install --cask iterm2
--- setup.sh
+++ setup.sh
@@ -3,17 +3,6 @@
 #Homebrewのインストール
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-#Google Chromeのインストール
-brew install --cask google-chrome
-
-#Visual Studio Codeのインストール
-brew install --cask visual-studio-code
-
-#Todoistのインストール
-brew install --cask todoist
-
-#Clipyのインストール
-brew install --cask clipy
-
-#iTerm2のインストール
-brew install --cask iterm2
+#デスクトップアプリケーションのインストール
+chmod +x ./customize/guiapp-install.sh
+./customize/guiapp-install.sh
