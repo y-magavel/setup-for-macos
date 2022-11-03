02ce57c52f0310e4891c432e22de1f8d905a3b1c
y-magavel <magavel.dev@gmail.com>
2022-11-03 18:35:24 +0900

    nodebrewのuse実行とパスを通すコマンドを追加
    .zshrcに下記の形式で追記
    
    # nodebrew
    export PATH=$HOME/.nodebrew/current/bin:$PATH

--- dev/target/install-node.sh
+++ dev/target/install-node.sh
@@ -6,3 +6,7 @@ cd $(dirname $0)
 brew install nodebrew
 mkdir -p ~/.nodebrew/src
 nodebrew install-binary latest
+nodebrew use latest
+
+# パスを通す
+echo '\n# nodebrew\nexport PATH=$HOME/.nodebrew/current/bin:$PATH' >> ~/.zshrc
