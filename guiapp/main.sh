a845a71a7bf9302053dee9868324fc9adf6b072e
y-magavel <magavel.dev@gmail.com>
2022-12-04 01:41:16 +0900

    iTerm2のインストールをコメントアウト
    ターミナルアプリをWarpに乗り換えるため

--- guiapp/main.sh
+++ guiapp/main.sh
@@ -5,7 +5,7 @@ cd $(dirname $0)
 ./target/install-google-chrome.sh
 ./target/install-vscode.sh
 ./target/install-clipy.sh
-./target/install-iterm2.sh
+#./target/install-iterm2.sh
 ./target/install-jetbrains-toolbox.sh
 ./target/install-tableplus.sh
 ./target/install-zoom.sh
